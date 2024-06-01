959a85cd80d18fd2c53fd33f6770e04e7357e8b5
Hamza Mehmood <mehmood.hamza777@gmail.com>
2024-06-01 06:38:25 +0500

    add new column with name of view

--- app/Http/Controllers/usercontroller.php
+++ app/Http/Controllers/usercontroller.php
@@ -7,22 +7,27 @@ use Illuminate\Support\Facades\DB;
 
 class usercontroller extends Controller
 {
-public function showuser(){
-   $users= DB::table('user')->get();
-  // return $users;
-    
+   public function showuser()
+   {
+      $users = DB::table('user')->get();
 
-/*  foreach ($users as $use) {
-   echo $use->email."<br>";
-  }*/
-
-    return view('allusers',['data'=>$users]);
-
-//dd( $users);
+        // return $users;
 
 
+      /*  foreach ($users as $use) {
+         echo $use->email."<br>";
+        }*/
 
-}
+      return view('allusers', ['data' => $users]);
+
+      //dd( $users);
 
 
+
+   }
+
+      public function singleuser(){
+         $users = DB::table('user')->where('email', )->get();
+         
+      }
 }
